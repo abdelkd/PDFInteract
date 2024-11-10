e6d00ed502836a6df104f2b030b58d8d4b2cb95c
abdelkd <abdelkarimdjelalda@gmail.com>
2024-11-10 20:00:59 +0100

    fix: env variables

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,10 @@
 import { defineConfig } from 'drizzle-kit';
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-if (process.env.VERCEL_URL && !process.env.POSTGRES_URL) throw new Error('POSTGRES_URL is not set');
+if (!process.env.DATABASE_URL && !process.env.POSTGRES_URL) throw new Error('DATABASE_URL or POSTGRES_URL must be set');
 
 export default defineConfig({
   schema: './src/lib/server/db/schema.ts',
-
   dbCredentials: {
-    url: process.env.VERCEL_URL ? process.env.POSTGRES_URL! :  process.env.DATABASE_URL
+    url: process.env.POSTGRES_URL! ??  process.env.DATABASE_URL!
   },
 
   verbose: true,
--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -4,9 +4,7 @@ import { env } from '$env/dynamic/private';
 import { drizzle as vercelDrizzle } from 'drizzle-orm/vercel-postgres';
 import { sql } from '@vercel/postgres';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-// throw an error if app is running on vercel but POSTGRES_URL is not set
-if (process.env.VERCEL_URL && !process.env.POSTGRES_URL) throw new Error('POSTGRES_URL is not set');
-const client = postgres(env.DATABASE_URL);
+if (!process.env.DATABASE_URL && !process.env.POSTGRES_URL) throw new Error('DATABASE_URL or POSTGRES_URL must be set');
 
-export const db = process.env.VERCEL_URL ? vercelDrizzle(sql) : drizzle(client);
+const client = postgres(env.DATABASE_URL);
+export const db = process.env.POSTGRES_URL ? vercelDrizzle(sql) : drizzle(client);
