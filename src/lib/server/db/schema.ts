419fd5551fae8a436ecefe1506232639eec252e4
abdelkd <abdelkarimdjelalda@gmail.com>
2024-11-14 22:59:15 +0100

    feat: add expiration schema

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,26 +1,28 @@
-import { pgTable, text, integer, timestamp, boolean } from 'drizzle-orm/pg-core';
-import { generateChatID } from '../chat';
+import { pgTable, text, integer, timestamp, date } from 'drizzle-orm/pg-core';
 
 export const user = pgTable('user', {
-    id: text('id').primaryKey(),
-    age: integer('age'),
-    username: text('username').notNull().unique(),
-    passwordHash: text('password_hash').notNull()
+	id: text('id').primaryKey(),
+	age: integer('age'),
+	username: text('username').notNull().unique(),
+	passwordHash: text('password_hash').notNull()
 });
 
-export const session = pgTable("session", {
-    id: text('id').primaryKey(),
-    userId: text('user_id').notNull().references(() => user.id),
-    expiresAt: timestamp('expires_at', { withTimezone: true, mode: 'date' }).notNull()
+export const session = pgTable('session', {
+	id: text('id').primaryKey(),
+	userId: text('user_id')
+		.notNull()
+		.references(() => user.id),
+	expiresAt: timestamp('expires_at', { withTimezone: true, mode: 'date' }).notNull()
 });
 
-export const chatTable = pgTable("chat", {
-    id: text('id').primaryKey().notNull(),
-    prompt: text('prompt').array().notNull(),
-    answer: text('answer').array().notNull(),
-    fileUri: text('file_Uri').notNull(),
-    initializedAnswer: text('initialized_answer'),
-})
+export const chatTable = pgTable('chat', {
+	id: text('id').primaryKey().notNull(),
+	prompt: text('prompt').array().notNull(),
+	answer: text('answer').array().notNull(),
+	fileUri: text('file_Uri').notNull(),
+	createdAt: timestamp('created_at', { withTimezone: true, mode: 'date' }).defaultNow().notNull(),
+    expiresOn: timestamp('expires_on', { withTimezone: true, mode: 'date' }).notNull(),
+});
 
 export type Session = typeof session.$inferSelect;
 
