d8c77aff465fb30bb780eee83b347b9ee2cd6815
abdelkd <abdelkarimdjelalda@gmail.com>
2024-12-20 12:36:08 +0100

    add montserrat font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
+import '@fontsource-variable/montserrat'
 	import '../app.css';
 	let { children } = $props();
 </script>
-
 
 <div class="w-screen h-dvh md:h-screen px-3">
 	{@render children()}
