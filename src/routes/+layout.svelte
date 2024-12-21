c507283952425670130a18f9bad1f2cd9123b4ca
abdelkd <abdelkarimdjelalda@gmail.com>
2024-12-21 23:15:03 +0100

    feat: add favicons, title and description

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,12 @@ import '@fontsource-variable/montserrat'
 	let { children } = $props();
 </script>
 
+<svelte:head>
+  <title>PDF Interact: AI-Powered PDF Q&A Tool</title>
+  <meta name="description" content="Unlock insights from PDFs with PDF Interact, an AI-powered tool for extracting answers to your questions. Perfect for research and study." />
+  <meta name="keywords" content="PDF question answering tool, extract information from PDF, get answers from PDFs, PDF research assistant, AI-powered PDF analysis, PDF content extraction tool, intelligent PDF question answering, PDF study aid, PDF AI interaction tool, PDF Q&A, efficient PDF research tool, quick PDF answer extraction">
+</svelte:head>
+
 <div class="w-screen h-dvh md:h-screen px-3">
 	{@render children()}
 </div>
